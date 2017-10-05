425cff828d9041775b54357efa2d55ae2b219ff3
drew23c <32339225+drew23c@users.noreply.github.com>
2017-10-05 01:09:32 -0400

    Add files via upload

---
+++ bball.js
@@ -0,0 +1,56 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+var playerOne ={
+    teamName: undefined,
+    teamPoints:0
+}
+var computer = {
+    teamName: 'Monstars',
+    teamPoints: 0
+}
+
+console.log('Time to play the MONSTARS');
+rl.setPrompt('Enter your team name');
+rl.prompt();
+
+var plays = [
+    'freeThrow', 'jumpShot', 'threePointShot'] 
+var calls=['WWWOOOOWWW','BOOM SHAKALAKA', 'You won\t see that everyday',
+'someone\s ankle is broken', 'Man DOWN']
+var madeShot =['made','missed']
+
+var rPlays = Math.floor(Math.random() * plays.length);
+var rCalls = Math.floor(Math.random() * calls.length);
+var rShot = Math.floor(Math.random() * madeShot.length);
+
+
+rl.on('line', function(input){
+    if(playerOne.teamName === undefined){
+        playerOne.teamName = input;
+    }
+    console.log('JUMPBALL')
+    var jumpBall = Math.floor(Math.floor(Math.random() * 5))    
+    if(jumpBall > 3){
+        console.log(`${playerOne.teamName}'s ball`)
+    }else{
+        console.log(`${computer.teamName} ball`)
+    }
+    var rShot = Math.floor(Math.random() * madeShot.length);
+    if(input.toLowerCase === 'shoot'){
+        var rPlays = Math.floor(Math.random() * plays.length);
+        console.log(`${rCalls}, ${rPlays} is ${rShot}`)
+        if(rPlays === 'freeThrow'){
+            playerOne.teamPoints += teamPoints + 1;
+        }else if(rPlays === 'jumpshot'){
+            playerOne.teamPoints += teamPoints + 2;
+        }else if(rPlays === 'threePointShot'){
+            playerOne.teamPoints += teamPoints + 3;
+        }
+    }
+    
+})
