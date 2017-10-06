bc49c4d45af127fc899cdbc617e76f929df01ac9
drew23c <32339225+drew23c@users.noreply.github.com>
2017-10-06 17:10:38 -0400

    Add files via upload

--- bballv2.js
+++ bballv2.js
@@ -21,7 +21,6 @@ var computer = {
 console.log('Time to play the MONSTARS\n A 3-on-3 basketball game\n First team to 21 points WINS');
 rl.setPrompt('Enter your team name');
 rl.prompt();
-var i = 0;
 
 
 rl.on('line', function(input){
@@ -31,10 +30,12 @@ rl.on('line', function(input){
     var madeShot =['made','missed']
     var tVt = [playerOne.teamName, computer.teamName]
     
-    var offensivePlay = ['shoot','dunk','3-pointer']
+    var offensivePlay = ['shoot','dunk','3']
     var defensivePlay = ['defend', 'steal']
+
+    var controls = Math.floor(Math.random() * 10);
     
-    var rPlays = Math.floor(Math.random() * plays[i]);
+    var rPlays = Math.floor(Math.random() * plays.length);
     var rCalls = Math.floor(Math.random() * calls.length);
     var rShot = Math.floor(Math.random() * madeShot.length);
     var vulnerD = Math.floor(Math.random() * 5);
@@ -43,181 +44,109 @@ rl.on('line', function(input){
     if(playerOne.teamName === undefined){
         playerOne.teamName = input;
     }
-    console.log('JUMPBALL')
-    var jumpBall = Math.floor(Math.random() * 5)   
-    if(jumpBall > 3){
-        console.log(`${playerOne.teamName}'s ball`)
-        rl.setPrompt('OFFENSE\n' + offensivePlay[0] + ' | ' + offensivePlay[1] + ' | ' + offensivePlay[2])
-        rl.prompt();
-        if(offensivePlay[0]){
-            clear();
-            rShot
-            if(rShot === 'made'){
-                console.log(`${rCalls}, jumpshot is made!!`)
-                playerOne.teamPoints +=  2;
-                console.log(`${computer.teamName} ${computer.teamPoints}`);
-                console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-            }else{
-                console.log(`jumpshot is missed!!`)
-                console.log(`${computer.teamName} ${computer.teamPoints}`);
-                console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                teamPool
-                if(teamPool > 2){
-                    console.log(`${computer.teamName}'s ball`) 
-                    rl.setPrompt('DEFESE\n' + defensivePlay[0] + ' | ' + defensivePlay[1])
-                    rl.prompt();            
-                }else{
-                    console.log(`${rCalls}, ${rPlays} is made`)
-                    if(rPlays === 'jumpshot'){
-                        playerOne.teamPoints +=  2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }else if(rPlays === 'threePointShot'){
-                        playerOne.teamPoints +=  3;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }else{
-                        playerOne.teamPoints +=  2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    } 
-                        }
-                    }
-                        
-        }
-        else if(offensivePlay[1]){
-            clear();
-            rShot
-            if(rShot === 'made'){
-                console.log(`${rCalls}, what a DUNK!!`)
-                playerOne.teamPoints +=  2;
-                console.log(`${computer.teamName} ${computer.teamPoints}`);
-                console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-            }else{
-                console.log(`OH MY!!, he spazzed on the dunk!!`)
-                console.log(`${computer.teamName} ${computer.teamPoints}`);
-                console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                teamPool
-                if(teamPool > 2){
-                    console.log(`${computer.teamName}'s ball`) 
-                    rl.setPrompt('DEFESE\n' + defensivePlay[0] + ' | ' + defensivePlay[1])
-                    rl.prompt();            
-                }else{
-                    console.log(`${rCalls}, ${rPlays} is made`)
-                    if(rPlays === 'jumpshot'){
-                        playerOne.teamPoints +=  2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                        }
-                    }
-                }
-        } else{
-            clear();
-            rShot
-            if(rShot === 'made'){
-                console.log(`${rCalls}, the 3 is made!!`)
-                playerOne.teamPoints +=  3;
-                console.log(`${computer.teamName} ${computer.teamPoints}`);
-                console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-            }else{
-                console.log(`The 3 is missed!!`)
-                console.log(`${computer.teamName} ${computer.teamPoints}`);
-                console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                teamPool
-                if(teamPool > 2){
-                    console.log(`${computer.teamName}'s ball`) 
-                    rl.setPrompt('DEFESE\n' + defensivePlay[0] + ' | ' + defensivePlay[1])
-                    rl.prompt();            
-                }else{
-                    console.log(`${rCalls}, ${rPlays} is made`)
-                    if(rPlays === 'jumpshot'){
-                        playerOne.teamPoints +=  2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                        }
-                    }
-            }
-        }  
+    clear();
+
+
+    if(input === 'dunk' && controls >= 4){ ///////SELECTED DUNK///////////
+        clear();
+        playerOne.teamPoints +=  2;
+            console.log(`BOOOM SHAKALAKA, I think the ground shook with that DUNK`)
+            console.log(`\n${computer.teamName} ${computer.teamPoints}`);
+            console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+            console.log(`${computer.teamName} ball\n`)
+            
+    
+    }else if(input === 'dunk' && controls < 4){
+        clear();
+        console.log(`\nOH MY!!, ${playerOne.teamName} spazzed on the dunk!!`)
+        console.log(`${computer.teamName} ${computer.teamPoints}`);
+        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+        
     }
-    else{
-        console.log(`${computer.teamName} ball`)
-        rl.setPrompt('DEFESE\n' + defensivePlay[0] + ' | ' + defensivePlay[1])
-        rl.prompt();
-        if(defensivePlay[1]){
-            clear();
-            var vulnerD = Math.floor(Math.random() * 5);
-                if(vulnerD > 2){
-                    console.log(`Matedore D has led to an EZ ${rPlays}`)
-                    if(rPlays === 'jumpshot'){
-                        computer.teamPoints += 2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }else if(rPlays === 'threePointShot'){
-                        computer.teamPoints +=  3;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }else{
-                        computer.teamPoints +=  2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }
-                    
-                }
-                rl.setPrompt('OFFENSE\n' + offensivePlay[0] + ' | ' + offensivePlay[1] + ' | ' + offensivePlay[2])
-                rl.prompt();
+    
+    if(input === 'shoot' && controls >= 5){/////////SELECTED SHOOT////////////
+        clear();
+        playerOne.teamPoints +=  2;
+            console.log(`Showing off the mid-range GAME`)
+            console.log(`\n${computer.teamName} ${computer.teamPoints}`);
+            console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+            
+    
+    }else if(input === 'shoot' && controls < 5){
+        clear();
+        console.log(`\nOH MY!!, the shot is no good!!`)
+        console.log(`${computer.teamName} ${computer.teamPoints}`);
+        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+       
+    }
+
+    if(input === '3' && controls >= 7){/////////3-POINT SHOT////////////
+        clear();
+        playerOne.teamPoints +=  3;
+            console.log(`From WWAAAYYYY DOWN TOWN`)
+            console.log(`\n${computer.teamName} ${computer.teamPoints}`);
+            console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+           
+    
+    }else if(input === '3' && controls < 7){
+        clear();
+        console.log(`\nOH MY!!, the 3 is no good!!`)
+        console.log(`${computer.teamName} ${computer.teamPoints}`);
+        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+       
+    }
+
+
+    if(input === 'defend' && controls >= 6){
+        clear();
+        computer.teamPoints +=  2;
+            console.log(`\n${computer.teamName} ${computer.teamPoints}`);
+            console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+           
+    
+    }else if(input === 'defend' && controls < 6){
+        clear();
+        console.log(`\nOH MY!!, the ${computer.teamName} dunk is no good!!`)
+        console.log(`${computer.teamName} ${computer.teamPoints}`);
+        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+    }
+
+    if(input === 'steal' && controls >= 4){
+        clear();
+        computer.teamPoints +=  2;
+            console.log(`That is something you don't see everyday, WWWOOOOWWWW!!!!`)
+            console.log(`\n${computer.teamName} ${computer.teamPoints}`);
+            console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+           
+    
+    }else if(input === 'steal' && controls < 4){
+        clear();
+        console.log(`\nOH MY!!, what a steal!! ${playerOne.teamName}'s ball!!`)
+        console.log(`${computer.teamName} ${computer.teamPoints}`);
+        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
+    }
+
+    if(computer.teamPoints >= 21){
+        console.log(`The ${computer.teamName} snatched your basketball soul, ${computer.teamPoints} to ${playerOne.teamPoints}`)
+        rl.close();
+    }else if(playerOne.teamPoints >= 21){
+        console.log(`You beat the ${computer.teamName}, ${playerOne.teamPoints} to ${computer.teamPoints}`)
+        rl.close();
+    }else{
+
+
+
+        
+        var jumpBall = Math.floor(Math.random() * 10 + 1)   
+        if(jumpBall >= 5){
+            console.log(`${playerOne.teamName}'s ball!!`)
+            rl.setPrompt(`OFFENSE\ndunk | shoot | 3-pointer `)
+            rl.prompt();
         }else{
-            clear();
-            vulnerD
-            if(vulnerD < 3){
-                console.log(`${playerOne.teamName} has defended the rim`)
-                teamPool
-                if(playerOne.teamName){
-                    console.log(`${playerOne.teamName} grabbed the rebound`)
-                    rl.setPrompt('OFFENSE\n' + offensivePlay[0] + ' | ' + offensivePlay[1] + ' | ' + offensivePlay[2])
-                    rl.prompt();
-                }else{
-                    console.log(`${computer.teamName} with the putback!!`)
-                    rPlays
-                    if(rPlays === 'jumpshot'){
-                        playerOne.teamPoints += 2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }else if(rPlays === 'threePointShot'){
-                        playerOne.teamPoints +=  3;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }else{
-                        playerOne.teamPoints +=  2;
-                        console.log(`${computer.teamName} ${computer.teamPoints}`);
-                        console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-                    }
-                }
-            }
-            rl.setPrompt('OFFENSE\n' + offensivePlay[0] + ' | ' + offensivePlay[1] + ' | ' + offensivePlay[2])
+            console.log(`${computer.teamName} ball!!`)
+            rl.setPrompt('DEFENSE\n defend | steal ')
             rl.prompt();
         }
     }
-    // var rShot = Math.floor(Math.random() * madeShot.length);
-    // if(input.toLowerCase === 'shoot'){
-    //     var rPlays = Math.floor(Math.random() * plays.length);
-    //     console.log(`${rCalls}, ${rPlays} is ${rShot}`)
-    //     console.log(`${computer.teamName} ${computer.teamPoints}`);
-    //     console.log(`${playerOne.teamName} ${playerOne.teamPoints}`);
-    //     if(rPlays === 'freeThrow' && rShot === 'made'){
-    //         playerOne.teamPoints += teamPoints + 1;
-    //     }else if(rPlays === 'jumpshot' && rShot === 'made'){
-    //         playerOne.teamPoints += teamPoints + 2;
-    //     }else if(rPlays === 'threePointShot' && rShot === 'made'){
-    //         playerOne.teamPoints += teamPoints + 3;
-    //     }
-    // }
-
-    // if(computer.teamPoints >= 21){
-    //     console.log(`You lost to the ${computer.teamName}, ${computer.teamPoints} to ${playerOne.teamPoints}`)
-    //     rl.close();
-    // }else{
-    //     console.log(`You beat the ${computer.teamName}, ${playerOne.teamPoints} to ${computer.teamPoints}`)
-    //     rl.close();
-    // }
     
 });
